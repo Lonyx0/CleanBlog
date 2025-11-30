25224b80779a8c1203e91eaa370392839ccf734a
Eren ALTAY <eren60.altay@gmail.com>
2025-11-30 17:30:36 +0300

    Refactor routing and controllers for posts and pages; add edit and delete functionality

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const methodOverride = require('method-override');
+
+const pageController = require('./controllers/pageController');
+const postController = require('./controllers/postController');
 require ('dotenv').config();
 
 const connectDB = require('./config/db');
@@ -7,38 +11,26 @@ const ejs = require('ejs');
 const app = express();
 connectDB();
 app.set('view engine', 'ejs');
-const Post = require('./models/Post');
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
+
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    const posts = await Post.find({});
-    res.render('index', {
-        posts
-    });
-});
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post('/posts', postController.createPost);
+app.delete('/posts/:id', postController.deletePost);
+app.put('/posts/:id', postController.updatePost);
 
-app.get('/posts/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id);
-    res.render('post', {
-        post
-    });
-})
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPostPage);
+app.get('/posts/edit/:id', pageController.getEditPostPage);
 
-app.get('/about', (req, res) => {
-    res.render('about');
-});
 
-app.get('/add_post', (req, res) => {
-    res.render('add_post');
-});
 
-app.post('/posts', async (req, res) => {
-    await Post.create(req.body);
-    res.redirect('/');
-})
+
 
 const port = 3000;
 app.listen(port, () => {
